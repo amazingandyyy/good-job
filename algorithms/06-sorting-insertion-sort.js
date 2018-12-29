2f6d5d073e0a5bd27396d71436b5573b935b49af
Andy Chen <amazingandyyy@gmail.com>
2018-12-29 14:38:26 -0800

    finish insertion sorting

--- algorithms/06-sorting-insertion-sort.js
+++ algorithms/06-sorting-insertion-sort.js
@@ -1,13 +1,17 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
-// [1,2,3,5,44,99,...]
+// [1,2,3,6,44,99,...]
 function insertionSort(array) {
  for(let i = 0; i<array.length; i++) {
   if(array[i] < array[0]) {
+   //move number to the first position
    array.unshift(array.splice(i, 1)[0])
   }else{
-   for(let j=i; j>0;i--) {
-    if(array[j]<array[i]){
-     
+   for(let j=i; j>0; j--) {
+    //find where number should go
+    if(array[i]>array[j-1] && array[i]<array[j]){
+     //move number to the right spot
+     let item = array.splice(i,1)[0]; // already chop off the item from the list
+     array.splice(j,0,item); // place it
     }
    }
   }
@@ -15,7 +19,7 @@ function insertionSort(array) {
 }
 
 insertionSort(numbers);
-console.log(numbers);
+console.log('numbers', numbers);
 // space: O(1)
 // time: O(N^2)
 // [ 0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283 ]
